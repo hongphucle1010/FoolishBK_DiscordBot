d4c2c17765b19b39613bc6da83d9192552074507
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2025-03-23 20:51:48 +0700

    GitHub init

---
+++ src/main/java/com/foolishbk/foolishbk/FoolishBkApplication.java
@@ -0,0 +1,12 @@
+package com.foolishbk.foolishbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoolishBkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoolishBkApplication.class, args);
+    }
+}
---
+++ src/test/java/com/foolishbk/foolishbk/FoolishBkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foolishbk.foolishbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoolishBkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
